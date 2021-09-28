1084040e5bee30ee7ab79e0892bdeace2b1e1939
roman.y <roman.yavoriv@didomi.io>
2021-09-28 17:29:32 +0300

    setup: add storybook

---
+++ packages/ui-library/src/components/my-component/my-component.stories.ts
@@ -0,0 +1,9 @@
+import { Meta } from '@storybook/html';
+
+export default {
+  title: 'My Component',
+} as Meta;
+
+export const MyComponent = () => {
+  return `<my-component first="Roman" last="Yavoriv"></my-component>`
+}
