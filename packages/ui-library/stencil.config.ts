65091d56741d72bc539fd0fcb16f8f5ce38c961f
roman.y <roman.yavoriv@didomi.io>
2021-09-29 15:33:41 +0300

    setup: add ui-library wrapper for Angular(draft)

--- packages/ui-library/stencil.config.ts
+++ packages/ui-library/stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
 
+import { angularOutputTarget } from '@stencil/angular-output-target';
+
 export const config: Config = {
   namespace: 'ui-library',
   outputTargets: [
+    angularOutputTarget({
+      componentCorePackage: 'ui-library',
+      directivesProxyFile: '../ui-library-angular/src/directives/proxies.ts',
+    }),
     {
       type: 'dist',
       esmLoaderPath: '../loader',
